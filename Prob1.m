8a282239f387bd7a3f7a119ba681324ec187d25a
RamosMariaKarina <47411234+RamosMariaKarina@users.noreply.github.com>
2019-12-08 00:28:50 -0800

    Add files via upload

---
+++ Prob1.m
@@ -0,0 +1,12 @@
+f=(0:99);   
+for n=1:100
+ if f(n)<=9
+     f(n)=(f(n))^2-7;
+ elseif f(n)>=10
+         f(n)=f(n-10);
+end
+end
+stem(f)
+xlabel('Value of n')
+ylabel('f(n)')
+title('Matlab Machine Problem1')
